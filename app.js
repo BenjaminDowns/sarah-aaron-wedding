9eeaf8139ccd9c9df14712e99542944d54c0806b
Benjamin Downs <benjamin.downs@gmail.com>
2015-09-17 10:22:39 +0200

    removed the logger for static files

--- app.js
+++ app.js
@@ -14,17 +14,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico')); //still not working!?!
-app.use(logger('dev'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(function(req,res,next){
-//     res.local.req = req;
-//     next();
-// })
+app.use(logger('dev'));
 
 
 app.use('/', routes);
